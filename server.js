7d754a4584d931baad79dbd1a81b294e0fd4fae7
codewithswatiee <verma.s@somaiya.edu>
2024-09-23 09:30:30 +0530

    setup done

--- server.js
+++ server.js
@@ -16,3 +16,12 @@ app.post('/api/cars', (req, res) => {
     console.log(name, year);
     res.send('Data Submitted!');
 })
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/myDatabase') //promise
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((error) => {
+    console.log(`Recieved ${error}`);
+})
