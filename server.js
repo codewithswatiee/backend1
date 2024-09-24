25683569bd6401b05ee19aa5dad56adf274d7af3
codewithswatiee <verma.s@somaiya.edu>
2024-09-24 18:29:13 +0530

    "Removed body-parser, changed port from 5000 to 3000, deleted routes and MongoDB connection"

--- server.js
+++ server.js
@@ -1,27 +1,6 @@
 const express = require('express');
 const app = express();
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.listen(5000, () => {
+app.listen(3000, () => {
     console.log('Server is running on port 3000');
-});
-
-app.get('/', (req, res) => {
-    res.send("Hello!");
-})
-
-app.post('/api/cars', (req, res) => {
-    const { name, year } = req.body;
-    console.log(name, year);
-    res.send('Data Submitted!');
-})
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/myDatabase') //promise
-.then(() => {
-    console.log('Connected to MongoDB');
-})
-.catch((error) => {
-    console.log(`Recieved ${error}`);
 })
